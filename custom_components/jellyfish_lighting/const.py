e4afcdaf8ecab8df62c20b88f72dcae57c41d6b5
Dave <ddbyro@gmail.com>
2025-08-08 23:35:57 -0600

    Initial commit

--- custom_components/jellyfish_lighting/const.py
+++ custom_components/jellyfish_lighting/const.py
@@ -5,6 +5,6 @@ DEFAULT_PORT = 9000
 CONF_HOST = "host"
 CONF_PORT = "port"
 
-# SERVICE_RUN_PATTERN = "run_pattern"
-# SERVICE_RUN_PATTERN_ADV = "run_pattern_advanced"
-# SERVICE_GET_PATTERN_DATA = "get_pattern_data"
+SERVICE_RUN_PATTERN = "run_pattern"
+SERVICE_RUN_PATTERN_ADV = "run_pattern_advanced"
+SERVICE_GET_PATTERN_DATA = "get_pattern_data"
