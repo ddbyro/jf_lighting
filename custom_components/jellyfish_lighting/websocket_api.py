9b56ad36ba790757fa0906b87a861f70ea8a9d61
Dave <ddbyro@gmail.com>
2025-08-08 23:40:15 -0600

    Initial commit

--- custom_components/jellyfish_lighting/websocket_api.py
+++ custom_components/jellyfish_lighting/websocket_api.py
@@ -9,6 +9,9 @@ from homeassistant.core import HomeAssistant, callback
 
 from .const import DOMAIN
 
+from homeassistant.helpers.dispatcher import async_dispatcher_send
+
+
 _LOGGER = logging.getLogger(__name__)
 
 class JellyfishClient:
@@ -94,15 +97,12 @@ class JellyfishClient:
 
         cmd = payload.get("cmd")
         if cmd == "fromCtlr":
-            # patternFileList
             if "patternFileList" in payload:
                 self._patterns = payload["patternFileList"]
-                # notify HA to update entities
-                self.hass.helpers.dispatcher.async_dispatcher_send(f"{DOMAIN}_patterns_updated")
+                async_dispatcher_send(self.hass, f"{DOMAIN}_patterns_updated")
             if "zones" in payload:
                 self._zones = payload["zones"]
-                self.hass.helpers.dispatcher.async_dispatcher_send(f"{DOMAIN}_zones_updated")
-        # other responses can be handled similarly
+                async_dispatcher_send(self.hass, f"{DOMAIN}_zones_updated")
 
     async def _send(self, payload: Dict[str, Any]):
         await self._connected_event.wait()
