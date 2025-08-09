858bbd1ca9acd51929e52769e1ffdda13371f48e
Dave <ddbyro@gmail.com>
2025-08-08 20:51:20 -0600

    Initial commit

--- custom_components/jellyfish_lighting/__init__.py
+++ custom_components/jellyfish_lighting/__init__.py
@@ -23,5 +23,5 @@ async def async_setup_entry(hass: HomeAssistant, entry) -> bool:
     """
     Store config entry data for platform setup.
     """
-    await hass.config_entries.async_forward_entry_setup(entry, "light")
+    await hass.async_forward_entry_setup(entry, "light")
     return True
